d44c0440ae221e07a3fb6f2ed747af48fa01b27a
Khun Yarzar <zar31528@gmail.com>
2025-01-08 08:29:48 +0630

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -17,10 +17,10 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::post('/auth/login', [AuthController::class, 'login']);
-Route::post('/auth/register', [AuthController::class, 'register']);
 
 Route::group(['middleware' => 'auth:api'], function () {
-
+    Route::post('/auth/register', [AuthController::class, 'register']);
+    
     // Category
     Route::post('/category', [CategoryController::class, 'store']);
     Route::get('/categories', [CategoryController::class, 'index']);
